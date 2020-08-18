63ac9dda9d2b05b17942d71b9d3048758b64cb86
Thisuraka <64680083+Thisuraka@users.noreply.github.com>
2020-08-18 21:41:48 +0530

    initial

---
+++ app/src/main/java/com/example/intentsproj/firstActivity.java
@@ -0,0 +1,37 @@
+package com.example.intentsproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class firstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.btn1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(firstActivity.this, secondActivity.class));
+                Context context = getApplicationContext();
+                CharSequence message = "You just clicked the OK button";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, message, duration);
+                toast.setGravity(Gravity.BOTTOM|Gravity.LEFT, 40, 100);
+                toast.show();
+            }
+        });
+
+
+    }
+}
