0584e25a90ab1f7167ddfe409164ec5754d9d7f6
Thisuraka <64680083+Thisuraka@users.noreply.github.com>
2020-08-19 00:23:26 +0530

    initial

--- app/src/main/java/com/example/intentsproj/firstActivity.java
+++ app/src/main/java/com/example/intentsproj/firstActivity.java
@@ -6,8 +6,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class firstActivity extends AppCompatActivity {
@@ -17,23 +20,29 @@ public class firstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-     //   View txtBx1 = findViewById(R.id.txtBx1);
-     //   View txtBx2 = findViewById(R.id.txtBx2);
-     //   View btn1 = findViewById(R.id.btn1);
+        LayoutInflater lay = getLayoutInflater();
+        final View layout = lay.inflate((R.layout.customtoast), (ViewGroup) findViewById(R.id.custom_toast_layout));
 
-        Button btn = (Button)findViewById(R.id.btn1);34
+        final EditText et1 = findViewById(R.id.txtBx1);
+        final EditText et2 = findViewById(R.id.txtBx2);
+        Button btn = (Button)findViewById(R.id.btn1);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(firstActivity.this, secondActivity.class));
-                Context context = getApplicationContext();
 
+                Context context = getApplicationContext();
                 CharSequence message = "You just clicked the OK button";
                 int duration = Toast.LENGTH_SHORT;
                 Toast toast = Toast.makeText(context, message, duration);
-                toast.setGravity(Gravity.BOTTOM|Gravity.LEFT, 40, 100);
+                toast.setGravity(Gravity.BOTTOM|Gravity.LEFT, 0, 0);
                 toast.show();
+
+                Intent intent = new Intent(getApplicationContext(),secondActivity.class);
+
+                intent.putExtra("value1",et1.getText().toString());
+                intent.putExtra("value2",et2.getText().toString());
+                 startActivity(intent);
             }
         });
 
